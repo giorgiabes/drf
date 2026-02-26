0298cf99b2d3a1a6019d5159ff3abaf78e571c5c
giorgiabes <abesadze.george@gmail.com>
2026-02-26 22:01:21 +0400

    hyperlink api

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -4,13 +4,18 @@ from rest_framework import serializers
 from .models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source="owner.username")
+    highlight = serializers.HyperlinkedIdentityField(
+        view_name="snippet-highlight", format="html"
+    )
 
     class Meta:
         model = Snippet
         fields = (
+            "url",
             "id",
+            "highlight",
             "title",
             "code",
             "linenos",
@@ -20,14 +25,15 @@ class SnippetSerializer(serializers.ModelSerializer):
         )
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(
-        many=True, queryset=Snippet.objects.all()
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(
+        many=True, view_name="snippet_detail", read_only=True
     )
 
     class Meta:
         model = User
         fields = (
+            "url",
             "id",
             "username",
             "snippets",
